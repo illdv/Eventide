a7e80f47d2533adcac1a3147e53c045559b206ed
Kirill Dvoynikov <illdv@mail.ru>
2017-07-08 19:06:42 +0300

    end 0.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
-$(function(){
-	
+$(function() {
+  $('.collapse').on('show.bs.collapse', function() {
+    $(this).prev().find('.glyphicon-plus-sign').removeClass('glyphicon-plus-sign').addClass('glyphicon-minus-sign')
+  });
+
+  $('.collapse').on('hide.bs.collapse', function() {
+    $(this).prev().find('.glyphicon-minus-sign').removeClass('glyphicon-minus-sign').addClass('glyphicon-plus-sign')
+  });
+  
+	$('#collapse1').collapse({
+  show: true
+});
+
+$('#register').validator({
+  feedback: {
+    success: 'glyphicon-ok-sign',
+    error: 'glyphicon-remove-sign'
+  }
+});
+
+$('.plan').select2({
+  minimumResultsForSearch: Infinity
+});
+
+new WOW({
+  offset: 100,
+}).init();
+
+$("body").on('click', '[href*="#"]', function(e){
+var fixed_offset = 100;
+$('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+e.preventDefault();
+});
 });
