8428cacfa48e32f033c901ff827775f664968762
Kirill Dvoynikov <illdv@mail.ru>
2017-07-09 14:19:14 +0300

    D

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-$(function(){$(".collapse").on("show.bs.collapse",function(){$(this).prev().find(".glyphicon-plus-sign").removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign")}),$(".collapse").on("hide.bs.collapse",function(){$(this).prev().find(".glyphicon-minus-sign").removeClass("glyphicon-minus-sign").addClass("glyphicon-plus-sign")}),$("#collapse1").collapse({show:!0}),$("#register").validator({feedback:{success:"glyphicon-ok-sign",error:"glyphicon-remove-sign"}}),$(".plan").select2({minimumResultsForSearch:1/0}),new WOW({offset:100}).init(),$("body").on("click",'a[href^="#u"]',function(s){$("html,body").stop().animate({scrollTop:$(this.hash).offset().top-100},1e3),s.preventDefault()}),$(document).ready(function(){$("head").append("<link rel='stylesheet' href='css/select2.min.css' />")})});
+$(function() {
+  $(".collapse").on("show.bs.collapse", function() {
+    $(this).prev().find(".glyphicon-plus-sign").removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign")
+  }), $(".collapse").on("hide.bs.collapse", function() {
+    $(this).prev().find(".glyphicon-minus-sign").removeClass("glyphicon-minus-sign").addClass("glyphicon-plus-sign")
+  }), $("#collapse1").collapse({
+    show: !0
+  }), $("#register").validator({
+    feedback: {
+      success: "glyphicon-ok-sign",
+      error: "glyphicon-remove-sign"
+    }
+  }),  new WOW({
+    offset: 100
+  }).init(), $("body").on("click", 'a[href^="#u"]', function(s) {
+    $("html,body").stop().animate({
+      scrollTop: $(this.hash).offset().top - 100
+    }, 1e3), s.preventDefault()
+  }), $(document).ready(function() {
+    $("head").append("<link rel='stylesheet' href='css/select2.min.css' />")
+  })
+});
